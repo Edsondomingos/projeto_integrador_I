e9e9dac769f918515527fd8f747f8bb77bb80b8f
Fabiana <fabianasouza012@gmail.com>
2022-03-09 00:21:26 -0300

    deletar perfil

--- _bd/deletar_perfil.php
+++ _bd/deletar_perfil.php
@@ -6,7 +6,7 @@ include "conexao.php";
 
 $conn = conectar();
 
-$sql = "DELETE FROM evento WHERE cpf=".$perfil;
+$sql = "DELETE FROM atleta WHERE cpf=".$perfil;
 
 if($conn->query($sql)){
     header ('location: perfil.php');
