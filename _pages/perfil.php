29caf6b5632607e7b108cc556b717e9148ed0207
adriana <adriana123ici@gmail.com>
2022-03-14 22:02:54 -0300

    perfil modificado

--- _pages/perfil.php
+++ _pages/perfil.php
@@ -45,12 +45,12 @@
 				while($row= $result->fetch_assoc()){
             
 						echo "<p>Cpf".$row['cpf']."</p>";
-						echo "Nome".$row['nome'];
-						echo "Data de Nascimento".$row["data_de_nascimento"];
-			            echo "Telefone".$row["telefone"];
-			            echo "E-Mail".$row["email"];
-			            echo "Usuário".$row["usuario"];
-			            echo "Senha".$row["senha"];
+						echo "<p>Nome".$row['nome']."<p>";
+						echo "<p>Data de Nascimento".$row["data_de_nascimento"]."</p>";
+			            echo "<p>Telefone".$row["telefone"]."</p>";
+			            echo "<p>E-Mail".$row["email"]."</p>";
+			            echo "<p>Usuário".$row["usuario"]"</p>;
+			            echo "<p>Senha".$row["senha"]."</p>";
 
 					};
 }
