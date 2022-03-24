9edbe42878a95127061e10409003f576b1516b53
Fabiana <fabianasouza012@gmail.com>
2022-03-24 11:42:02 -0300

    Paginas editar_evento e editar_evento_bd editadas

--- _bd/editar_evento_bd.php
+++ _bd/editar_evento_bd.php
@@ -1,5 +1,6 @@
 <?php
 
+$id = $_GET["id"];
 $local_evento = $_POST["local_evento"];
 $data_evento = $_POST["data_evento"];
 $imagem = $_POST["imagem"];
@@ -13,7 +14,7 @@ include "conexao.php";
 
 $conn = conectar();
 
-$sql = "UPDATE evento SET titulo='".$titulo."' , local_evento='".$local_evento."' , data_evento='".$data_evento."' , imagem='".$imagem."' , descricao='".$descricao."' WHERE correr=$correr";
+$sql = " UPDATE evento SET titulo='".$titulo."', local_evento='".$local_evento."' , data_evento='".$data_evento."', imagem='".$imagem."', descricao='".$descricao."', valor='".$valor."'  WHERE id=.$id ";
 
 if($conn->query($sql)){
     header ('location: ../_pages/criar_evento.php');
